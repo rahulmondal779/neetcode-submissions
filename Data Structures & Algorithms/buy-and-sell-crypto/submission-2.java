32993042d044f7b616abd2308808b6d85ea0b346
Rahul Mondal <rahulmondal779@gmail.com>
2026-05-15 02:08:07 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int buy = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            if (buy > prices[i]) {
+                buy = prices[i];
+            }
+            maxProfit = Math.max(maxProfit, prices[i]-buy);   
+        }
+
+        return maxProfit;
+    }
+}
